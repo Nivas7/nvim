49f81f12c464807d6792dc01834e819233f1212c
Srinivas <nivasniyas24@gmail.com>
2025-02-23 20:01:30 +0530

    ...WIP

--- lua/plugins/grug-far.lua
+++ lua/plugins/grug-far.lua
@@ -1,9 +1,10 @@
 -- NOTE: Find And Replace
 return {
-	"MagicDuck/grug-far.nvim",
-	cmd = "GrugFar",
-	init = function()
-		vim.keymap.set("n", "<leader>fR", "<cmd>GrugFar<cr>", { desc = "GrugFar | Find And Replace", silent = true })
-	end,
-	opts = {},
+  "MagicDuck/grug-far.nvim",
+  event = "VeryLazy",
+  cmd = "GrugFar",
+  init = function()
+    vim.keymap.set("n", "<leader>fR", "<cmd>GrugFar<cr>", { desc = "GrugFar | Find And Replace", silent = true })
+  end,
+  opts = {},
 }
