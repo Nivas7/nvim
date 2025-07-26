f81e6bd80037c5ffbb1f8e7cbe4f2d588229a14e
Srinivas <nivasniyas24@gmail.com>
2025-07-26 16:11:39 +0530

    Regular Checks

--- init.lua
+++ init.lua
@@ -4,8 +4,8 @@ require("keymaps")
 require("statusline")
 require("statuscolumn")
 
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+---@diagnostic disable-next-line: undefined-field
 if not vim.loop.fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
