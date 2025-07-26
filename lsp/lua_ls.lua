44c7ad6b47c973f86d0cbf74b641ad7612385f93
Srinivas <nivasniyas24@gmail.com>
2025-07-26 16:10:22 +0530

    small dignostics fix

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -15,6 +15,7 @@ return {
 			local path = client.workspace_folders[1].name
 			if
 				path ~= vim.fn.stdpath("config")
+				---@diagnostic disable-next-line: undefined-field
 				and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
 			then
 				return
