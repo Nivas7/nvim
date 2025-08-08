e821688e9c024be204628e616018bcea258a5f90
Srinivas <nivasniyas24@gmail.com>
2025-08-08 11:17:06 +0530

    style(oraganize configurations): orgniazing..

---
+++ lua/config/options.lua
@@ -0,0 +1,72 @@
+-- Pre-requisites
+local o = vim.opt
+vim.g.mapleader = " "
+
+-- General
+o.undofile = true
+o.cursorline = false
+o.termguicolors = true
+o.clipboard = "unnamedplus"
+o.scrolloff = 5
+
+-- netrw-customization
+vim.g.netrw_keepdir = 0
+vim.g.netrw_winsize = 20
+vim.g.netrw_banner = 0
+vim.g.netrw_localcopydircmd = "cp -r"
+
+-- disable nvim intro
+o.shortmess:append("sI")
+
+-- Statusline
+o.showmode = true
+
+-- Indenting
+o.shiftwidth = 2
+o.tabstop = 2
+o.softtabstop = 2
+o.smartindent = true
+o.autoindent = true
+o.expandtab = true
+
+-- Enable filetype-specific plugins and indentation
+-- o.filetype = "on"
+-- o.filetype.plugin = "on"
+-- o.filetype.indent = "on"
+
+o.winborder = "rounded"
+
+o.ignorecase = true
+o.smartcase = true
+o.mouse = "a"
+o.fillchars = { eob = " " }
+
+-- Number
+o.number = true
+o.relativenumber = true
+
+-- Column
+o.signcolumn = "yes"
+o.splitbelow = true
+o.splitright = true
+o.timeoutlen = 400
+
+-- Character limit column
+o.colorcolumn = "80"
+
+-- Disable colorcolumn for markdown
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.colorcolumn = ""
+  end,
+})
+
+-- go to previous/next line with h,l,left arrow and right arrow
+o.whichwrap:append("<>[]hl")
+
+-- Automatic :retab on save (fix indentation errors)
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  command = "retab",
+})
