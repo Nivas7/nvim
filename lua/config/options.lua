022cba863973f8262e1bd86c0388c5bb45fe2569
Srinivas <nivasniyas24@gmail.com>
2025-08-09 10:13:00 +0530

    ci(tweaks in options and keymaps):

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,72 +1,67 @@
--- Pre-requisites
-local o = vim.opt
-vim.g.mapleader = " "
 
--- General
-o.undofile = true
-o.cursorline = false
-o.termguicolors = true
-o.clipboard = "unnamedplus"
-o.scrolloff = 5
+local options = {
+  backup = false, -- creates a backup file
+  autochdir = false,
+  cmdheight = 1, -- more space in the neovim command line for displaying messages
+  completeopt = {
+    "menuone",
+    "popup",
+    "noselect", --[["noinsert", "fuzzy"]]
+  }, -- mostly just for completion plugins
+  conceallevel = 0, -- so that `` is visible in markdown files
+  fileencoding = "utf-8", -- the encoding written to a file
+  hlsearch = true, -- highlight all matches on previous search pattern
+  ignorecase = true, -- ignore case in search patterns
+  mouse = "a", -- allow the mouse to be used in neovim
+  pumheight = 10, -- pop up menu height
+  showmode = false, -- we don't need to see things like -- INSERT -- anymore
+  smartcase = true, -- smart case
+  smartindent = true, -- make indenting smarter again
+  splitbelow = true, -- force all horizontal splits to go below current window
+  splitright = true, -- force all vertical splits to go to the right of current window
+  swapfile = false, -- creates a swapfile
+  termguicolors = true, -- set term gui colors (most terminals support this)
+  timeoutlen = 500, -- time to wait for a mapped sequence to complete (in milliseconds)
+  undofile = true, -- enable persistent undo
+  updatetime = 300, -- faster completion (4000ms default)
+  writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+  expandtab = true, -- convert tabs to spaces
+  shiftwidth = 4, -- the number of spaces inserted for each indentation
+  tabstop = 4, -- insert 2 spaces for a tab
+  cursorline = true, -- highlight the current line
+  number = true, -- set numbered lines
+  relativenumber = true, -- set relative numbered lines
+  numberwidth = 2, -- set number column width to 2 {default 4}
+  signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
+  wrap = false, -- display lines as one long line
+  scrolloff = 8, -- is one of my fav
+  guifont = "JetBrainsMono NF:h14", -- the font used in graphical neovim applications
+  sidescrolloff = 8, -- the number of lines to keep visible at the top and bottom of the screen
+  laststatus = 3, -- controls how the command line looks when there is no message to display
+  virtualedit = "onemore", -- allows the cursor to move beyond the end of a line
+  linebreak = true, -- determines whether text will wrap at the edge of the screen
+  showtabline = 1, -- determines whether the tab line will be displayed
+  spelllang = "en_us", -- sets the language for spell checking
+  textwidth = 80, -- limits the width of text that is being inserted
+  foldtext = "",
+  foldexpr = "v:lua.vim.treesitter.foldexpr()", -- specifies the expression used to calculate folds
+  indentexpr = "nvim_treesitter#indent()", -- specifies the function used to calculate the indentation level
+  fillchars = { eob = " ", fold = " " }, -- defines the characters used for certain parts of the screen
+  wildoptions = { "fuzzy", "pum", "tagfile" },
+  -- messagesopt = "wait:10000,history:1000",
+  splitkeep = "screen",
+  inccommand = "split",
+}
 
--- netrw-customization
-vim.g.netrw_keepdir = 0
-vim.g.netrw_winsize = 20
-vim.g.netrw_banner = 0
-vim.g.netrw_localcopydircmd = "cp -r"
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
 
--- disable nvim intro
-o.shortmess:append("sI")
-
--- Statusline
-o.showmode = true
-
--- Indenting
-o.shiftwidth = 2
-o.tabstop = 2
-o.softtabstop = 2
-o.smartindent = true
-o.autoindent = true
-o.expandtab = true
-
--- Enable filetype-specific plugins and indentation
--- o.filetype = "on"
--- o.filetype.plugin = "on"
--- o.filetype.indent = "on"
-
-o.winborder = "rounded"
-
-o.ignorecase = true
-o.smartcase = true
-o.mouse = "a"
-o.fillchars = { eob = " " }
-
--- Number
-o.number = true
-o.relativenumber = true
-
--- Column
-o.signcolumn = "yes"
-o.splitbelow = true
-o.splitright = true
-o.timeoutlen = 400
-
--- Character limit column
-o.colorcolumn = "80"
-
--- Disable colorcolumn for markdown
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  callback = function()
-    vim.opt_local.colorcolumn = ""
-  end,
-})
-
--- go to previous/next line with h,l,left arrow and right arrow
-o.whichwrap:append("<>[]hl")
-
--- Automatic :retab on save (fix indentation errors)
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  command = "retab",
-})
+vim.opt.formatoptions:remove({ "c", "r", "o" })
+vim.opt.iskeyword:append("-")
+vim.opt.whichwrap:append("<,>,[,],h,l")
+vim.opt.diffopt:append("linematch:60")
+vim.opt.shortmess:append({ C = true, c = true, I = true })
+vim.opt.cinkeys:remove(":")
+vim.opt.indentkeys:remove(":")
+vim.g.c_syntax_for_h = true
