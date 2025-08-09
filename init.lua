536523b26cea973a55b7fb095f5a56b0c78bb189
Srinivas <nivasniyas24@gmail.com>
2025-08-09 10:12:02 +0530

    ci(small tweaks): \

--- init.lua
+++ init.lua
@@ -1,33 +1,9 @@
+vim.loader.enable()
+
 require("core.mason-path")
 require("core.lsp")
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
+require("core.lazy")
 
-vim.cmd("set completeopt+=noselect")
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
----@diagnostic disable-next-line: undefined-field
-if not vim.loop.fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		error("Error cloning lazy.nvim:\n" .. out)
-	end
-end
-
----@type vim.Option
-local rtp = vim.opt.rtp
-rtp:prepend(lazypath)
-
-require("lazy").setup({
-	spec = { import = "plugins" },
-	install = { colorscheme = { "habamax" } },
-	checker = {
-		enabled = false,
-		concurrency = nil, ---@type number? set to 1 to check for updates very slowly
-		notify = true, -- get a notification when new updates are found
-		frequency = 3600, -- check for updates every hour
-		check_pinned = false, -- check for pinned packages that can't be updated
-	},
-})
